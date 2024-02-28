c0f8b3e48d182dbf15dbfb19002fd539310877de
Justin Hiemstra <jhiemstra@wisc.edu>
2024-02-28 23:16:00 +0000

    Fix OB1 error

--- src/S3Commands.cc
+++ src/S3Commands.cc
@@ -388,7 +388,7 @@ AmazonS3Download::~AmazonS3Download() { }
 bool AmazonS3Download::SendRequest( off_t offset, size_t size ) {
 	if( offset != 0 || size != 0 ) {
 		std::string range;
-		formatstr( range, "bytes=%zu-%zu", offset, offset + size );
+		formatstr( range, "bytes=%zu-%zu", offset, offset + size -1 );
 		headers["Range"] = range.c_str();
 		this->expectedResponseCode = 206;
 	}
